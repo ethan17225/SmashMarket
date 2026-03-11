f1ac77756a2cbae2ebec8d1fcb457b012f0c58bb
The Hai Nguyen <thehai.nguyen.dev@gmail.com>
2026-03-10 20:08:33 -0400

    Initial Commit

---
+++ SmashMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmashMarket
+//
+//  Created by Ethan Nguyen on 2026-03-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
